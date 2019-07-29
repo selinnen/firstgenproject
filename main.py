47743c70ad88c0d7c3c18f381e9b1ff851052b56
cssi <cssi@cssis-MacBook-Pro-63.local>
2019-07-29 09:39:34 -0700

    main.py file

---
+++ main.py
@@ -0,0 +1,19 @@
+import webapp2
+import os
+import jinja2
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):  # for a get request
+
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/memepage', MemePage)
+], debug=True)
